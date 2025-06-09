6ab567c5dca80f0eaac1608b2af294d2dacc1b54
jotave400 <joaovictore368@gmail.com>
2025-06-09 06:13:55 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('#fotos #galeria').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300,
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+    });
+});
